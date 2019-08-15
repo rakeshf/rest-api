c2d1ffa91b8944cb980a5a4c770dbd1650eb29f5
Rakesh Falke <rakeshfalke@gmail.com>
2019-08-15 10:39:46 +0000

    Added model and router

--- app.js
+++ app.js
@@ -2,17 +2,23 @@ require('dotenv/config');
 const express = require('express');
 const mongoose = require('mongoose');
 const postsRoute = require('./routes/posts');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 
+//Middlewares
+app.use(cors());
+app.use(bodyParser.json());
 app.use('/posts', postsRoute);
 
-mongoose.connect(process.env.DB_CONNECTION, 
+
+mongoose.connect(process.env.DB_CONNECTION,
 { useNewUrlParser: true },
 () =>{
     console.log('connected to db');
 });
-//Middlewares
+
 /*app.use('/posts', () => {
     console.log('we hit the middleware');
     return true;
--- routes/posts.js
+++ routes/posts.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const routes = express.Router();
+const Post = require('../models/Post');
+
 //Routes
 routes.get('/', (req, res) => {
     res.send('We are at post index');
+});
+
+routes.post('/', async (request, response) => {
+    const post = new Post({
+        title: request.body.title,
+        description: request.body.description
+    });
+    try{
+        const postSaved = await post.save();
+        response.json(postSaved);
+    }catch(err){
+        response.json({message: err});
+    }
 });
 
 module.exports = routes;
