7c0d4807e551413e2a603aa96d66743990057686
Rakesh Falke <rakeshfalke@gmail.com>
2019-08-17 18:35:10 +0530

    Updated curd for post

--- routes/posts.js
+++ routes/posts.js
@@ -12,6 +12,39 @@ routes.get('/', async (request, response) => {
     }
 });
 
+// Get post
+routes.get('/:postId', async (request, response) => {
+    try{
+        const post = await Post.findById(request.params.postId); 
+        response.json(post);
+    }catch(err){
+        response.json({message: err});
+    }
+});
+
+// Delete
+routes.delete('/:postId', async (request, response) => {
+    try{
+        console.log('Delete post ID = '+request.params.postId);
+        const removedPost = await Post.deleteOne({_id: request.params.postId}); 
+        response.json(removedPost);
+    }catch(err){
+        response.json({message: err});
+    }
+});
+
+// Update
+routes.patch('/:postId', async (request, response) => {
+    try{
+        console.log('Update post ID = '+request.params.postId);
+        const updatePost = await Post.updateOne({ _id: request.params.postId }, 
+            { $set: { title:request.body.title } }); 
+        response.json(updatePost);
+    }catch(err){
+        response.json({message: err});
+    }
+});
+
 routes.post('/', async (request, response) => {
     const post = new Post({
         title: request.body.title,
